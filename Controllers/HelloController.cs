10cb1418e3a981b33d1ca97398b9338891289234
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2020-10-07 09:36:27 +0600

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("{firstName}/{lastName}")]
+        public String SayHello(string firstName, string lastName)
+        {
+
+            Student st = new Student
+            {
+                Name = firstName,
+                Surname = lastName
+            };
+            return "Hello " + st;
+        }
+    }
+}
